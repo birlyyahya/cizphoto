7de173e9863a646183a0e9207389c485c010ff05
birlyyahya <birlypecker123@gmail.com>
2025-03-20 13:00:41 +0700

    perbaikan index dan photo margin top

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,12 @@
 <x-layouts.guest>
-    <div class="flex flex-col items-center justify-center md:m-30 md:p-10 h-fit space-y-4 text-center z-20 absolute md:relative left-1/2 md:left-0 md:top-0 top-1/2 transform -translate-x-1/2 md:-translate-y-0 md:-translate-x-0 -translate-y-1/2">
+    <div class="flex flex-col items-center w-full justify-center h-fit space-y-4 text-center z-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
         <div class="flex justify-center items-center gap-4 md:gap-8">
             <flux:subheading size="xl" class="italic">est</flux:subheading>
             <div class="text-center text-5xl md:text-7xl italic">cizzphoto</div>
             <flux:subheading size="xl" class="italic">2025</flux:subheading>
         </div>
-        <p size="sm" class="text-sm mb-10 w-full m-auto text-gray-700">Ini websitenya cizburgerzz selamat fotoooo yangg cantikssssss
+        <p size="sm" class="text-sm mb-10 w-2/3 md:w-full m-auto text-gray-700">Ini websitenya cizburgerzz selamat fotoooo yangg cantikssssss
         </p>
-        {{-- <div class="m-10"> --}}
-            {{-- <flux:button variant="primary" href="/photo">Let’s Go!</flux:button> --}}
-        {{-- </div> --}}
         <flux:modal.trigger name="delete-profile">
             <flux:button variant="primary">Let's Go!</flux:button>
         </flux:modal.trigger>
--- resources/views/photo.blade.php
+++ resources/views/photo.blade.php
@@ -1,5 +1,5 @@
 <x-layouts.guest>
-    <div class="mb-10 md:p-10 relative  z-40 min-h-[800px]">
+    <div class="mb-10 mt-10 md:mt-0 md:p-10 relative z-40 min-h-[800px]">
         <div class="w-full md:max-w-4xl mx-auto p-6 rounded-xl bg-white shadow-md">
             <div class="mb-4 text-center">
                 <h2 class="text-2xl font-bold text-gray-800 mb-2">Cizzphoto</h2>
