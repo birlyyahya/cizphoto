65c05c6a57178c7aea95cfb591ba0cd029471a5d
birlyyahya <birlypecker123@gmail.com>
2025-03-20 17:56:56 +0700

    perubahan logo dan ukuran description pada halaman photo

--- resources/views/photo.blade.php
+++ resources/views/photo.blade.php
@@ -2,8 +2,8 @@
     <div class="mb-10 mt-10 md:mt-0 md:p-10 relative z-40 min-h-[800px]">
         <div class="w-full md:max-w-4xl mx-auto p-6 rounded-xl bg-white shadow-md">
             <div class="mb-4 text-center">
-                <h2 class="text-2xl font-bold text-gray-800 mb-2">Cizzphoto</h2>
-                <p class="text-gray-600">You have 3 seconds for each shot – no retakes!</p>
+                <img src="{{ asset('Image/logoCizz.png') }}" class="m-auto md:w-1/5 w-1/2" alt="" srcset="">
+                <p class="text-pink-600 md:text-sm text-xs">You have 3 seconds for each shot – no retakes!</p>
             </div>
             <livewire:photo-capture>
         </div>
