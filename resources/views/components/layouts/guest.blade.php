50b0629a1e7e47a7a24e5ea82cf0f48d54d61d75
birlyyahya <birlypecker123@gmail.com>
2025-03-20 17:48:40 +0700

    Ubah capital url link image

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.app.general>
     <flux:main>
-        <img src="{{asset('image/pink.webp')}}" class="blur-xl z-10 absolute w-full md:w-2/5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-70 rounded-full bg-contain bg-no-repeat bg-center" alt="background" srcset="">
+        <img src="{{asset('Image/pink.webp')}}" class="blur-xl z-10 absolute w-full md:w-2/5 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-70 rounded-full bg-contain bg-no-repeat bg-center" alt="background" srcset="">
         <flux:navbar class="justify-center gap-5 z-20 relative">
             <flux:navbar.item href="{{route('home')}}" :current="request()->routeIs('home') || request()->routeIs('photo')">Home</flux:navbar.item>
             <flux:navbar.item href="{{ route('privacy') }}" :current="request()->routeIs('privacy')">Privacy Policy</flux:navbar.item>
